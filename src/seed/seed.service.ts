9e1d9c2b1dcc9c393c13154b30be2675a6c59aa2
Michael <mikel.ibkc@gmail.com>
2024-03-22 02:11:33 -0600

    avance inserción por lote y delete anteriores

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -10,20 +10,28 @@ export class SeedService {
   private readonly axios: AxiosInstance = axios;
   constructor(
     @InjectModel( Pokemon.name)
-    private readonly PokemonModel: Model<Pokemon>
+    private readonly pokemonModel: Model<Pokemon>
   ){
   }
 
   async executeSeed(){
+    await this.pokemonModel.deleteMany({});
     const {data} = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=15');
 
-    data.results.forEach(async({name, url})=>{
+    const insertPromisesArrar = [];
+
+    data.results.forEach(({name, url})=>{
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
 
-      const pokemon = await this.PokemonModel.create({name, no});
-      
+      // const pokemon = await this.PokemonModel.create({name, no});
+      insertPromisesArrar.push(
+        this.pokemonModel.create({name, no})
+      );
     });
+
+
+    await Promise.all(insertPromisesArrar);
 
     return 'Seed executed';
   }
